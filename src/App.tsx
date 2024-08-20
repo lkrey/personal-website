75e6a14016859286501de1c1c524f9e82c4ee153
Laura Krey <laurakrey@gmail.com>
2024-08-20 19:16:04 -0400

    creates header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from '../public/logo192.png'
-import './App.css';
+import React, { Fragment } from "react";
+import { Grid } from "@mui/material";
+/*
+TODO: 
+ import { useRouter } from "next/router";
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo.src} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { headerContainer } = {
+  headerContainer: {
+    padding: "1rem 3rem",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+};
+
+interface Tab {
+  tabName: string;
+  onClick: () => void;
 }
+
+const App: React.FC = () => {
+
+  const tabs: Tab[] = [
+    {
+      tabName: "ABOUT",
+      onClick: () => {},
+    },
+    {
+      tabName: "INTERESTS",
+      onClick: () => {},
+    },
+    {
+      tabName: "ACCOMPLISHMENTS",
+      onClick: () => {},
+    },
+    {
+      tabName: "EXPERIENCE",
+      onClick: () => {},
+    },
+    {
+      tabName: "GET IN TOUCH",
+      onClick: () => {},
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Grid
+        component="header"
+        data-testid="app-header-container"
+        style={headerContainer}
+      >
+        {tabs.map((tab: Tab, index: number) => (
+          <Grid key={index} onClick={() => tab.onClick()} item>
+            {tab.tabName}
+          </Grid>
+        ))}
+      </Grid>
+    </Fragment>
+  );
+};
 
 export default App;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from 'next';
-import '../index.css';
- 
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'React App',
-  description: 'Web site created with Next.js.',
-}
+  title: "Laura Krey",
+  description: "Laura Krey's personal website",
+};
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-   <html lang="en">
-  <head>
-    <title>React App</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root">{children}</div>
-  </body>
-</html>
-    )
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Laura Krey</title>
+      </head>
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
 }
